4559ab185fce5db3cd453dc2f9d9941d9aff3972
Ratan Sen <ratansen.iitg@gmail.com>
2025-05-06 21:54:36 +0530

    Added cache basic pointer implementation

---
+++ cache.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+
+using namespace std;
+
+class EvictionPolicy{
+public:
+    virtual void keyAccessed() = 0;
+    virtual void evictKey() = 0;
+    virtual void say() = 0;
+};
+
+class LRUEvictionPolicy : public EvictionPolicy{
+private:
+
+
+public: 
+
+    LRUEvictionPolicy(){
+        cout << "LRUEvictionPolicy constructor" << endl;
+    }
+
+    void say() override{
+        cout << "Hi from LRU Eviction policy" << endl;
+    }
+
+    void keyAccessed() override{
+
+    }
+
+    void evictKey() override{
+
+    }
+};
+
+class Storage{
+
+public:
+    virtual int get(int key) = 0;
+    virtual void put(int key, int value) = 0;
+    virtual void remove(int key) = 0;
+    virtual void say() = 0;
+};
+
+class HashMapStorage: public Storage{
+private:
+
+public:
+    HashMapStorage(){
+        cout << "HashmapStorage constructor" << endl;
+    }
+
+    void say() override{
+        cout << "Hi from HashMapStorage" << endl;
+    }
+
+    int get(int key) override{
+        
+    }
+
+    void put(int key, int value) override{
+    }
+
+    void remove(int key) override{
+
+    }
+
+};
+
+class Cache{
+private:
+public:
+    int capacity;
+    EvictionPolicy* evictionPolicy;
+    Storage* storage;
+    Cache(int capacity, EvictionPolicy* evictionPolicy, Storage* storage){
+        this -> capacity = capacity;
+        this -> evictionPolicy = evictionPolicy;
+        this -> storage = storage;
+    }
+};
+
+
+int main(){
+    Cache* c = new Cache(10, new LRUEvictionPolicy(), new HashMapStorage());
+    c -> evictionPolicy -> say(); 
+    c -> storage -> say();
+
+}
